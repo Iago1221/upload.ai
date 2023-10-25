fc306a043c6687e7e74cb2cbd25d764560e74d76
Iago <gy24388@gmail.com>
2023-10-24 21:01:32 -0300

    Realinhamento de branch

---
+++ upload.ai/src/lib/ffmpeg.ts
@@ -0,0 +1,30 @@
+import { FFmpeg } from '@ffmpeg/ffmpeg'
+
+import coreURL from '../ffmpeg/ffmpeg-core.js?url'
+import wasmURL from '../ffmpeg/ffmpeg-core.wasm?url'
+import workerURL from '../ffmpeg/ffmpeg-worker.js?url'
+
+let ffmpeg: FFmpeg | null
+
+export async function getFFmpeg() {
+    if (ffmpeg) {
+        return ffmpeg
+    }
+
+    try{
+        ffmpeg = new FFmpeg()
+
+        if(!ffmpeg.loaded) {
+            await ffmpeg.load({
+                coreURL,
+                wasmURL,
+                workerURL
+            })
+        }
+
+        return ffmpeg
+    }catch (error) {
+        console.error("Error loading ffmpeg:", error);
+        throw error;
+    }
+}
